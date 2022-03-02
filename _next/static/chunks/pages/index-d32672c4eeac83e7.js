af8b5e459e2af8b4a9117af25556b7a7b77ef7fb
Peter <lisbon@ivysaur.co>
2022-03-02 09:57:43 +0000

    deployed from plogue-web

--- _next/static/chunks/pages/index-d32672c4eeac83e7.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{764:function(n,u,t){"use strict";t.r(u),t.d(u,{default:function(){return _.default}});var _=t(8342)},8581:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return t(764)}])}},function(n){n.O(0,[342,774,888,179],(function(){return u=8581,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
